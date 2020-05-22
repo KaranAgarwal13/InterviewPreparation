d18cde4d0c98d955cc69722b73d3f29f1dd7f738
Karan Agarwal <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-23 00:47:11 +0530

    Create jumpingOnClouds.cpp

---
+++ HackerRank/Algorithms/jumpingOnClouds.cpp
@@ -0,0 +1,10 @@
+int jumpingOnClouds(vector<int> c, int k) {
+    int loc = 0, energy = 100;
+    do{
+        energy--;
+        loc = (loc+k)%c.size();
+        //avoid if statement for branch prediction failure
+        energy -= 2*c[loc];
+    }while(loc);
+    return energy;
+}
