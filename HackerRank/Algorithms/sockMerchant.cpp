3e189116dbc33f4951f496ee008381a43b61efd5
Karan Agarwal <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-22 18:16:22 +0530

    Update sockMerchant.cpp

--- HackerRank/Algorithms/sockMerchant.cpp
+++ HackerRank/Algorithms/sockMerchant.cpp
@@ -1,9 +1,20 @@
 int sockMerchant(int n, vector<int> ar) {
-    int res = 0;
+    //Using Bitset to reduce runtime substantially
+    bitset<100> sockDrawer(0);
+    int pairs = 0;
+    for(int color : ar)
+    {
+        if(sockDrawer.test(color))  pairs++;
+        sockDrawer.flip(color);
+    }
+    return pairs;
+/*
+    int pairs = 0;
+    //Implementing HashTable
     int a[101]= {0};
     for(int i = 0; i < n; i++)
         a[ar[i]]++;
     for(int i = 1; i < 101; i++)
-        res += a[i]/2;
-    return res;
+        pairs += a[i]/2;
+    return pairs;*/
 }
