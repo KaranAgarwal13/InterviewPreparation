7d19b26f76ba32f67caa557359d84cc29433d4aa
Karan Agarwal <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-28 04:10:09 +0530

    Create cutTheSticks.cpp

---
+++ HackerRank/Algorithms/cutTheSticks.cpp
@@ -0,0 +1,14 @@
+vector<int> cutTheSticks(vector<int> arr) {
+    //sort in nlogn time
+    sort(arr.begin(), arr.end());
+    //vector to track sticks left
+    vector<int> sticksLeft;
+    //before beginning obviously all sticks will be left
+    sticksLeft.push_back(arr.size());
+    for(int i = 0; i < arr.size()-1; i++)
+        //trigger only if stick size isn't same as next one
+        if(arr[i] != arr[i+1])
+            //pushback numberOfSticks - numberOfSticksCut
+            sticksLeft.push_back(arr.size()-(1+&arr[i]-&arr[0]));
+    return sticksLeft;
+}
