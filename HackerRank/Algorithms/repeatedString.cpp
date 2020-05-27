401917eadd466a9d2cc46153b34bcd74e159d0eb
Karan Agarwal <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-28 03:24:24 +0530

    Update repeatedString.cpp

--- HackerRank/Algorithms/repeatedString.cpp
+++ HackerRank/Algorithms/repeatedString.cpp
@@ -1,10 +1,19 @@
-long (string s, long n) {
+long repeatedString(string s, long n) {
     vector<int> locations;
     for(char& c: s)
         if(c=='a')  locations.push_back(&c - &s[0]);
+    if(!locations.size())//if a is not at all present
+        return 0;
+    //multiply number of a's * string repetition
     long count = locations.size()*(n/s.length());
-    int charCount = n%s.length();
-    int index = locations.size()-1;
-    while(locations[index])
+    int charCount = n%s.length();//track remaining partial string length 
+    if(charCount)//skip if no partial string
+    {
+        int index = locations.size()-1;//get index for locations of a
+        while(locations[index]>charCount-1)//search how many a's come
+            index--;
+        if(index>=0)//if a's are present
+            count += index+1;
+    }
     return count;
 }
