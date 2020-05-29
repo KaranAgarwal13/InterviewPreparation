86e1710aa8d2f347b272f494a415371ca77e1614
Karan Agarwal <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-29 23:39:50 +0530

    Create serviceLane.cpp

---
+++ HackerRank/Algorithms/serviceLane.cpp
@@ -0,0 +1,11 @@
+vector<int> serviceLane(int n, vector<vector<int>> cases, vector<int> width) {
+    vector<int> largestVehicle(n);
+    for(int i = 0; i < n; i++)
+    {
+        if(cases[i][0]==i)
+            largestVehicle[i] = width[i];
+        if(cases[i][0]<i && cases[i][1]>=i && largestVehicle[i]>width[i])
+            largestVehicle[i] = width[i];
+    }
+    return largestVehicle;
+}
